b2dda1d1454794b4fabdbf96b042a66f2a3dd49e
Cyn108 <aishwaryanaga.31@gmail.com>
2026-07-12 12:03:05 +0530

    Add Tailwind CSS configuration file

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/*/.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0f172a",
+        secondary: "#3b82f6",
+        accent: "#10b981",
+      },
+    },
+  },
+  plugins: [],
+};
