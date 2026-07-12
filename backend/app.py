1fc3f787c2d3c8899f442d76b32ae2f84c74d940
Cyn108 <aishwaryanaga.31@gmail.com>
2026-07-12 16:16:44 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -74,5 +74,5 @@ def get_expenses():
     } for e in expenses])
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    
+    app.run(host="0.0.0.0", port=5000)
